d552a3c574ffd63b5afeb8fc532c2fc08ccbc226
Igor Shklyar <igor.shklyar@gmail.com>
2018-02-12 23:22:08 +0200

    256 Mapping Getters to Properties. Using mapGetters. The problem is that we can't use another computed properties.

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
-    <p>Counter is: {{ counter }}</p>
-    <p>Number of Clicks: {{ clicks }}</p>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of Clicks: {{ stringCounter }}</p>
   </div>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex';
+
   export default {
-    computed: {
-      counter() {
-        return this.$store.getters.doubleCounter;
-      },
-      clicks() {
-        return this.$store.getters.stringCounter;
+    computed: mapGetters(
+        // [ 'doubleCounter', 'stringCounter' ]
+        // we can also pass an Object and map the Getters to different Names
+      {
+        doubleCounter: 'doubleCounter',
+        stringCounter: 'stringCounter'
       }
-    }
+    )
   }
 </script>
