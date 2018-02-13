4a69ef312d7afd5e767bfa290213c481d29b7533
Igor Shklyar <igor.shklyar@gmail.com>
2018-02-13 23:25:18 +0200

    257 Using Mutations. The problem is we repeat our code

--- src/components/AnotherCounter.vue
+++ src/components/AnotherCounter.vue
@@ -6,14 +6,14 @@
 </template>
 
 <script>
-    export default {
-        methods: {
-            increment() {
-                this.$store.commit('increment');
-            },
-            decrement() {
-              this.$store.commit('decrement');
-            }
+export default {
+    methods: {
+        increment() {
+            this.$store.commit('increment');
+        },
+        decrement() {
+            this.$store.commit('decrement');
         }
     }
+}
 </script>
